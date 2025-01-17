182a20565650eb5748511e59392c46955fc33daf
zekima <christianledaal@gmail.com>
2025-01-17 16:02:09 +0100

    Setup next-intl (i18n)

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,36 @@
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
+import { Inter } from "next/font/google";
+import '../globals.css';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default async function LocaleLayout({
+    children,
+    params
+}: {
+    children: React.ReactNode;
+    params: {locale : string}
+}) {
+
+    const { locale } = await params;
+
+
+    if (!routing.locales.includes(locale as any)) {
+        notFound();
+    }
+
+    const messages = await getMessages();
+
+    return (
+        <html lang={locale}>
+            <body className={`${inter.className} antialiased`}>
+                <NextIntlClientProvider messages={messages}>
+                    {children}
+                </NextIntlClientProvider>
+            </body>
+        </html>
+    );
+}
