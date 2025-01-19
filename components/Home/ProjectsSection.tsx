c2ecf1fab7f84a08a40f72e3cd7f5f0fd1e54704
zekima <christianledaal@gmail.com>
2025-01-19 18:19:00 +0100

    Add Bachelor Project

--- components/Home/ProjectsSection.tsx
+++ components/Home/ProjectsSection.tsx
@@ -10,6 +10,18 @@ export default function ProjectsSection() {
                 <div className="border-b-2 py-3" />
             </div>
 
+            <div className="bg-secondary w-full h-96 flex gap-4 ">
+                <div className="w-1/2">
+                    <img src="/images/dugnadnett.webp" alt="" className="h-full"/>
+                </div>
+                <div className="w-1/2 pt-16 space-y-6 pr-16">
+                    <p className="text-5xl font-medium">DugnadNett</p>
+                    <p className="text-xl ">En digital plattform utviklet for å gjøre organisering av dugnader enklere og mer effektiv. Løsningen tilbyr sanntidskommunikasjon, smarte filterfunksjoner og en sentral oversikt over lokale dugnader.</p>
+                    <button className="bg-primary py-3 px-4 text-lg font-medium text-black">Les mer -{">"}</button>
+                </div>
+
+            </div>
+
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full">
                 <div className="bg-secondary h-64 flex justify-center items-center">
                 </div>
